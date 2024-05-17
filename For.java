6f2c6faeaa525c7a64c921a2543937a17790b434
sourojitdey <166232399+sourojitdey@users.noreply.github.com>
2024-05-17 09:15:19 +0530

    Delete For.java

--- For.java
+++
@@ -1,8 +0,0 @@
-class For {
-    public static void main(String arg[]) {
-        for (int i = 0; i <= 108; i++) {
-            System.out.println(
-                    "Ankita please sorry vul hoeageche please bojh ektu ki situation chilam please khoma korede amar oirokom behave korata uchit hoi ni..");
-        }
-    }
-}
